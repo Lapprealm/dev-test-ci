5d6585f6aa7e8d7bbe310c8e6879f695d535a4aa
irvanmalik48 <irvanmalik48@gmail.com>
2021-04-12 11:23:57 +0700

    create: update front end
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- application/views/hmif/create.php
+++ application/views/hmif/create.php
@@ -14,49 +14,211 @@
                     echo "</div>";
                 }
             ?>
+            <div class="row gy-4 mt-2">
+                <div class="card rounded-5 p-0">
+                    <h5 class="card-header">Identitas</h5>
+                    <div class="card-body">
+                        <div class="row gy-2">
+                            <div class="col-xs-4 col-sm-4 col-md-4">
+                                <div class="input-group">
+                                    <div class="input-group-text">Nama</div>
+                                    <input type="text" name="nama" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-xs-4 col-sm-4 col-md-4">
+                                <div class="input-group">
+                                    <div class="input-group-text">NIM</div>
+                                    <input type="text" name="nim" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-xs-4 col-sm-4 col-md-4">
+                                <div class="input-group">
+                                    <div class="input-group-text">Kelas</div>
+                                    <input type="text" name="kelas" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="card rounded-5 p-0">
+                    <h5 class="card-header">Survey #1</h5>
+                    <div class="card-body">
+                        <div class="row gy-2">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <p>
+                                    Ini pertanyaan
+                                </p>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-check">
+                                    <div class="row">
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="reaction" id="marah">
+                                            <label class="form-check-label" for="marah">
+                                                😤 (Mengmarah)
+                                            </label>
+                                        </div>
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="reaction" id="sedih">
+                                            <label class="form-check-label" for="sedih">
+                                                🙁 (Mengsedih)
+                                            </label>
+                                        </div>
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="reaction" id="datar">
+                                            <label class="form-check-label" for="datar">
+                                                😐 (Mengoke)
+                                            </label>
+                                        </div>
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                                🙂 (Mengsenang)
+                                            </label>
+                                        </div>
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                                😆 (Menghayyuk)
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <p class="mb-2 mt-2 h6">Nama</p>
-                        <input type="text" name="nama" class="form-control">
+                <div class="card rounded-5 p-0">
+                    <h5 class="card-header">Survey #2</h5>
+                    <div class="card-body">
+                        <div class="row gy-2">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <p>
+                                    Ini pertanyaan
+                                </p>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-check">
+                                    <div class="row">
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="reaction" id="marah">
+                                            <label class="form-check-label" for="marah">
+                                                😤 (Mengmarah)
+                                            </label>
+                                        </div>
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="reaction" id="sedih">
+                                            <label class="form-check-label" for="sedih">
+                                                🙁 (Mengsedih)
+                                            </label>
+                                        </div>
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="reaction" id="datar">
+                                            <label class="form-check-label" for="datar">
+                                                😐 (Mengoke)
+                                            </label>
+                                        </div>
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                                🙂 (Mengsenang)
+                                            </label>
+                                        </div>
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                                😆 (Menghayyuk)
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card rounded-5 p-0">
+                    <h5 class="card-header">Survey #3</h5>
+                    <div class="card-body">
+                        <div class="row gy-2">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <p>
+                                    Ini pertanyaan
+                                </p>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-check">
+                                    <div class="row">
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="reaction" id="marah">
+                                            <label class="form-check-label" for="marah">
+                                                😤 (Mengmarah)
+                                            </label>
+                                        </div>
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="reaction" id="sedih">
+                                            <label class="form-check-label" for="sedih">
+                                                🙁 (Mengsedih)
+                                            </label>
+                                        </div>
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="reaction" id="datar">
+                                            <label class="form-check-label" for="datar">
+                                                😐 (Mengoke)
+                                            </label>
+                                        </div>
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                                🙂 (Mengsenang)
+                                            </label>
+                                        </div>
+                                        <div class="col-xs col-sm col-md">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                            <label class="form-check-label" for="flexRadioDefault1">
+                                                😆 (Menghayyuk)
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card rounded-5 p-0">
+                    <h5 class="card-header">Form Aspirasi</h5>
+                    <div class="card-body">
+                        <div class="row gy-2">
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <p class="h6">Keluhan</p>
+                                    <textarea name="keluhan" class="form-control"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="row">
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <p class="h6">Kritik</p>
+                                            <textarea name="kritik" class="form-control"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-6 col-sm-6 col-md-6">
+                                        <div class="form-group">
+                                            <p class="h6">Saran</p>
+                                            <textarea name="saran" class="form-control"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <p class="mb-2 mt-2 h6">NIM</p>
-                        <input type="text" name="nim" class="form-control">
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <p class="mb-2 mt-2 h6">Kelas</p>
-                        <input type="text" name="kelas" class="form-control">
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <p class="mb-2 mt-2 h6">Keluhan</p>
-                        <textarea name="keluhan" class="form-control"></textarea>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <p class="mb-2 mt-2 h6">Kritik</p>
-                        <textarea name="kritik" class="form-control"></textarea>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <p class="mb-2 mt-2 h6">Saran</p>
-                        <textarea name="saran" class="form-control"></textarea>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <a 
-                    class="btn btn-primary mb-2 mt-2" 
-                    href="
+                    <a class="btn btn-primary mb-2 mt-2" href="
                     <?php 
                         echo base_url('hmif'); 
                         $this->session->sess_destroy(); 
