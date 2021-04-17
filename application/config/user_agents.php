5da2a45ce946fdc964bf16f976e4fe3bf8699a66
irvanmalik48 <irvanmalik48@gmail.com>
2021-04-17 16:37:19 +0700

    config: nuke out comments
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- application/config/user_agents.php
+++ application/config/user_agents.php
@@ -5,10 +5,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | -------------------------------------------------------------------
 | USER AGENT TYPES
 | -------------------------------------------------------------------
-| This file contains four arrays of user agent data. It is used by the
-| User Agent Class to help identify browser, platform, robot, and
-| mobile device data. The array keys are used to identify the device
-| and the array values are used to set the actual name of the item.
 */
 $platforms = array(
 	'windows nt 10.0'	=> 'Windows 10',
