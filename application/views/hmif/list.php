6f7408da72cf194523f21d5c0bd6da7bab0e70ef
irvanmalik48 <irvanmalik48@gmail.com>
2021-04-17 11:23:44 +0700

    tampung_aspirasi: edit: revamp
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- application/views/hmif/list.php
+++ application/views/hmif/list.php
@@ -1,7 +1,7 @@
 <main class="container">
-    <div class="row">
+    <div class="row mb-3 mt-3">
         <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
+            <div class="pull-left mb-3">
                 <h2>List</h2>
             </div>
             <div class="pull-right">
@@ -10,37 +10,19 @@
         </div>
     </div>
 
-    
-    <table class="table table-bordered">
+    <?php foreach ($data as $item) { ?>
+        <div class="card rounded-5 p-0 mb-3">
+            <div class="card-body d-flex justify-content-between align-items-center">
+                <p class="p-0 m-0 text-wrap">
+                    <?php echo $item->nim ?> - <?php echo $item->nama ?>
+                </p>
+                <form class="btn-group" method="DELETE" action="<?php echo base_url('hmif/delete/'.$item->id);?>">
+                    <a class="btn btn-primary" href="<?php echo base_url('hmif/'.$item->id) ?>">Show</a>
+                    <a class="btn btn-primary" href="<?php echo base_url('hmif/edit/'.$item->id) ?>">Edit</a>
+                    <button type="submit" class="btn btn-primary">Delete</button>
+                </form>
+            </div>
+        </div>
 
-
-    <thead>
-        <tr>
-            <th>Nama</th>
-            <th>NIM</th>
-            <th>Kelas</th>
-            <th width="220px">Action</th>
-        </tr>
-    </thead>
-
-
-    <tbody>
-    <?php foreach ($data as $item) { ?>      
-        <tr>
-            <td><?php echo $item->nama; ?></td>
-            <td><?php echo $item->nim; ?></td>   
-            <td><?php echo $item->kelas; ?></td>     
-        <td>
-            <form method="DELETE" action="<?php echo base_url('hmif/delete/'.$item->id);?>">
-                <a class="btn btn-info" href="<?php echo base_url('hmif/'.$item->id) ?>"> Show</a>
-                <a class="btn btn-primary" href="<?php echo base_url('hmif/edit/'.$item->id) ?>"> Edit</a>
-                <button type="submit" class="btn btn-danger"> Delete</button>
-            </form>
-        </td>     
-        </tr>
-        <?php } ?>
-    </tbody>
-
-
-    </table>
+    <?php } ?>
 </main>
