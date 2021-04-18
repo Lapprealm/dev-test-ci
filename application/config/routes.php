74ad9204daa39ba0eda61f375eb61ef659d3ecdf
irvanmalik48 <irvanmalik48@gmail.com>
2021-04-18 12:33:04 +0700

    routes: merge controller changes
    Signed-off-by: irvanmalik48 <irvanmalik48@gmail.com>

--- application/config/routes.php
+++ application/config/routes.php
@@ -10,9 +10,9 @@ $route['default_controller'] = 'main';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$route['hmif'] = "hmif/index";
-$route['hmif/(:num)'] = "hmif/show/$1";
-$route['hmifCreate']['post'] = "hmif/store";
-$route['hmifEdit/(:any)'] = "hmif/edit/$1";
-$route['hmifUpdate/(:any)']['put'] = "hmif/update/$1";
-$route['hmifDelete/(:any)']['delete'] = "hmif/delete/$1";
+$route['ta'] = "ta/index";
+$route['ta/(:num)'] = "ta/show/$1";
+$route['taCreate']['post'] = "ta/store";
+$route['taEdit/(:any)'] = "ta/edit/$1";
+$route['taUpdate/(:any)']['put'] = "ta/update/$1";
+$route['taDelete/(:any)']['delete'] = "ta/delete/$1";
